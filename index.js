f830032bf91a51c6dde68c83a53895bca74f9abb
Sonam Penjore <sonampenjoredolkar@gmail.com>
2022-08-06 23:50:24 +0700

    set up basic express app

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello')
+require('dotenv')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (_, res) => {
+  res.send('testing')
+})
+
+const PORT = process.env.PORT || 3030
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
